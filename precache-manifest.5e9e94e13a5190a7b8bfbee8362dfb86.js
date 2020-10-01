a6d720ae30e72d4a6a62eece86a6187878a59e90
umakiran9 <umakiran271@gmail.com>
2020-10-01 16:06:19 +0530

    Updates

---
+++ precache-manifest.5e9e94e13a5190a7b8bfbee8362dfb86.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5dd14f97b5206c409612bfdefbdea89",
+    "url": "/IMDB-Movie-App/index.html"
+  },
+  {
+    "revision": "faf3d6979bee4d32e772",
+    "url": "/IMDB-Movie-App/static/css/main.107e4302.chunk.css"
+  },
+  {
+    "revision": "c8c2ce442c716aa5b220",
+    "url": "/IMDB-Movie-App/static/js/2.88b3cfb8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/IMDB-Movie-App/static/js/2.88b3cfb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf3d6979bee4d32e772",
+    "url": "/IMDB-Movie-App/static/js/main.0bc3f7c4.chunk.js"
+  },
+  {
+    "revision": "76497c172e00cd855af1",
+    "url": "/IMDB-Movie-App/static/js/runtime-main.2f7afcb1.js"
+  }
+]);
